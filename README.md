e51ea0434713953dd7bcb6242eb809258a9d3bf1
nicolas2lee <nicolas2lee@gmail.com>
2020-02-28 15:10:42 +0100

    [doc] update

--- README.md
+++ README.md
@@ -30,5 +30,5 @@ kafka-console-producer --broker-list kafka-cp-kafka:9092  --topic bpp01bdd.db001
 
 
 /Users/xinrui/tao/software/confluent-5.3.1/bin/connect-standalone \
-/Users/xinrui/tao/apps/github/kafka-event-stream-mq-connect/eventstream/default-event-stream.properties \
+/Users/xinrui/tao/apps/github/kafka-event-stream-mq-connect/eventstream/event-stream-connector-standalone.properties \
 /Users/xinrui/tao/apps/github/kafka-event-stream-mq-connect/eventstream/connector-console-sink.properties
--- mq-producer/src/main/java/tao/MqspringApplication.java
+++ mq-producer/src/main/java/tao/MqspringApplication.java
@@ -1,13 +1,24 @@
 package tao;
 
+import com.ibm.jms.JMSTextMessage;
+import com.ibm.mq.MQMessage;
+import com.ibm.mq.headers.MQRFH2;
+import com.ibm.mq.pcf.CMQC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.io.IOException;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -22,14 +33,33 @@ public class MqspringApplication {
     }
 
     @GetMapping("send")
-    String send(){
+    String send() throws IOException, JMSException {
+
         try{
-            jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+            jmsTemplate.convertAndSend("DEV.QUEUE.1", "it is a test", new MessagePostProcessor() {
+                @Override
+                public Message postProcessMessage(Message message) throws JMSException {
+                    message.setStringProperty("xtestheader", "a ref");
+                    return message;
+                }
+            });
             return "OK";
         }catch(JmsException ex){
             ex.printStackTrace();
             return "FAIL";
         }
+    }
+
+    @JmsListener(destination = "DEV.QUEUE.1")
+    public void receivedMessage(Message message){
+        Optional.ofNullable(message).ifPresent(msg-> {
+            System.out.println(msg.toString());
+            try {
+                System.out.println(msg.getStringProperty("xtestheader"));
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
